1e77c3ab3228d658c7838df485938f9d1a304a30
awaaa <hawa.esanda@gmail.com>
2024-05-27 13:58:47 +0700

    C. UPLOAD FILE VALIDATION

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -12,26 +12,32 @@ class FileUploadController extends Controller
     }
     public function prosesFileUpload(Request $request)
     {
+//A
+        // return "Pemrosesan file upload disini";
+//B
         // dump($request->berkas);
         // dump($request->file('file'));
-        // return "Pemrosesan file upload disini";
-        if($request->hasFile('berkas'))
-        {
-            echo "path(): ".$request->berkas->path();
-            echo "<br>";
-            echo "extension: ".$request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): ".$request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): ".$request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): ".$request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): ".$request->berkas->getSize();
-        }
-        else
-        {
-            echo "Tidak ada berkas yang diupload";
-        }
+        // if($request->hasFile('berkas'))
+        // {
+        //     echo "path(): ".$request->berkas->path();
+        //     echo "<br>";
+        //     echo "extension: ".$request->berkas->extension();
+        //     echo "<br>";
+        //     echo "getClientOriginalExtension(): ".$request->berkas->getClientOriginalExtension();
+        //     echo "<br>";
+        //     echo "getMimeType(): ".$request->berkas->getMimeType();
+        //     echo "<br>";
+        //     echo "getClientOriginalName(): ".$request->berkas->getClientOriginalName();
+        //     echo "<br>";
+        //     echo "getSize(): ".$request->berkas->getSize();
+        // }
+        // else
+        // {
+        //     echo "Tidak ada berkas yang diupload";
+        // }
+//C
+        $request->validate([
+            'berkas'=>'required|file|image|max:500',]);
+        echo $request->berkas->getClientOriginalName()."<br>lolos validasi";
     }
 }
